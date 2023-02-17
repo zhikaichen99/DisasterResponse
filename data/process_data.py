1a44e895b44e84697e92ae500222f5e0ff31bbcf
zhikaichen99 <zhikaichen1999@gmail.com>
2023-02-16 23:53:45 -0500

    fix issue with replace database table if exists

--- data/process_data.py
+++ data/process_data.py
@@ -72,7 +72,7 @@ def save_data(df, database_filename):
         None
     """
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('DisasterMessagesTable', engine, index = False)
+    df.to_sql('DisasterMessagesTable', engine, index = False, if_exists = 'replace')
 
 
 def main():
